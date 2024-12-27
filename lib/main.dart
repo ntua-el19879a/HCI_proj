86e370ee75968aa040960300b2aaef97ecb381b7
Efthymios Karagiannis <ekaragiannis@outlook.com>
2024-12-27 16:10:12 +0200

    Add firebase core plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:prioritize_it/providers/task_provider.dart';
 import 'package:prioritize_it/providers/user_provider.dart';
-import 'package:prioritize_it/services/database_service.dart';
+// import 'package:prioritize_it/services/database_service.dart';
 import 'package:prioritize_it/services/notification_service.dart';
 import 'package:provider/provider.dart';
 import 'app.dart';
 import 'package:prioritize_it/providers/theme_provider.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize services with error handling
   try {
-    await DatabaseService.initialize();
-    print("DatabaseService initialized successfully.");
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    // await DatabaseService.initialize();
+    debugPrint('Database successfully initialized');
   } catch (e) {
-    print("Error initializing DatabaseService: $e");
+    debugPrint("Error initializing database: $e");
     return; // Consider alternative error handling (e.g., show error screen)
   }
 
   try {
     await NotificationService.initialize();
-    print("NotificationService initialized successfully.");
+    debugPrint("NotificationService initialized successfully.");
   } catch (e) {
-    print("Error initializing NotificationService: $e");
+    debugPrint("Error initializing NotificationService: $e");
     // You might want to handle this differently, e.g., by disabling
     // notifications or showing a warning to the user
   }
